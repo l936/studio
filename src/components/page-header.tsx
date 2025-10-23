05afbb62f55e0f044c8ad4c6244d0297540e4e6f
MD Tarek Sikder <tareksikder210@gmail.com>
2025-10-23 19:31:21 +0000

    ফ্রি 25GB Internet! সমস্ত নেটওয়ার্কে এখনই উপভোগ করুন। (_for element h1_)

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -5,7 +5,7 @@ export function PageHeader() {
   const headerImage = PlaceHolderImages.find((p) => p.id === 'hero-banner');
   return (
     <div className="bg-primary text-primary-foreground text-center p-3 space-y-2">
-      <h1 className="text-2xl font-bold">ফ্রি 100GB Internet! সমস্ত নেটওয়ার্কে এখনই উপভোগ করুন!</h1>
+      <h1 className="text-2xl font-bold">ফ্রি 25GB Internet! সমস্ত নেটওয়ার্কে এখনই উপভোগ করুন।</h1>
       {headerImage && (
         <Image
           src={headerImage.imageUrl}
