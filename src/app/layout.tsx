b09804a77f24eca80329176c02680df7e5083c0b
MD Tarek Sikder <tareksikder210@gmail.com>
2025-10-23 20:08:26 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Free 100GB Internet Offer',
@@ -21,8 +22,8 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        <script type='text/javascript' src='//pl27913869.effectivegatecpm.com/2e/6d/1f/2e6d1f6505e0e8a2e0b933d1b2be7d9b.js'></script>
-        <script type='text/javascript' src='//pl27913892.effectivegatecpm.com/48/b9/69/48b9693353acc2d3e518bf4868749521.js'></script>
+        <Script type='text/javascript' src='//pl27913869.effectivegatecpm.com/2e/6d/1f/2e6d1f6505e0e8a2e0b933d1b2be7d9b.js'></Script>
+        <Script type='text/javascript' src='//pl27913892.effectivegatecpm.com/48/b9/69/48b9693353acc2d3e518bf4868749521.js'></Script>
       </head>
       <body className="font-body antialiased">
         {children}
