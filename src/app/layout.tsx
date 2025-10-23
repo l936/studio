fce6507f038e6420a5921b24a854fa8cbe35d982
MD Tarek Sikder <tareksikder210@gmail.com>
2025-10-23 19:48:10 +0000

    এই এডটা সেট করে দেও <script type='text/javascript' src='//pl27913869.eff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <script type='text/javascript' src='//pl27913869.effectivegatecpm.com/2e/6d/1f/2e6d1f6505e0e8a2e0b933d1b2be7d9b.js'></script>
       </head>
       <body className="font-body antialiased">
         {children}
