acd9e3ac3bb7fee32b5413ce2d4c2e4f68a0e684
MD Tarek Sikder <tareksikder210@gmail.com>
2025-10-23 18:45:26 +0000

    https://i.ibb.co.com/9H1Pnmmx/file-000000002be861fa816deb850c15af40-1.pn

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -4,7 +4,7 @@ export function PageHeader() {
   return (
     <div className="bg-primary text-primary-foreground text-center p-3">
       <Image
-        src="https://i.ibb.co/9H1Pnmm/file-000000002be861fa816deb850c15af40-1.png"
+        src="https://i.ibb.co/9H1Pnmmx/file-000000002be861fa816deb850c15af40-1.png"
         alt="Free internet offer"
         width={750}
         height={400}
